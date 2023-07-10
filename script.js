2c28a9bdadca91a2a39546d245d0040e7a4b0e43
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-10 20:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+let change = document.getElementByTagName("strong") ;
+	for(let i=0; i < change.length; i++){
+		change[i].style.color = "green" ;
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+    let change = document.getElementByTagName("strong") ;
+	for(let i=0; i < change.length; i++){
+		change[i].style.color = "black" ;
+	}
 
-    
 }
